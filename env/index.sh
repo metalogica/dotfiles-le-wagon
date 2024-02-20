71fef5bcc8d4cd0f728993855968ce88bf11e506
richard jarram <rjarram@me.com>
2024-02-20 15:11:30 +0900

    cleanup aliases

---
+++ env/index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CODE_DIR="/Users/richardjarram/code"
+export CODE_DOTFILES_DIR="$CODE_DIR/metalogica/dotfiles"
+
+# shellcheck source=/Users/richardjarram/code/metalogica/dotfiles/env/global.sh
+source "$CODE_DOTFILES_DIR/env/global.sh"
--- zshrc.sh
+++ zshrc.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
+# env vars
+source "$HOME/code/metalogica/dotfiles/env/index.sh"
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
 export XDG_CONFIG_HOME="$HOME/.config/"
@@ -19,16 +21,16 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Useful plugins for zsh
 plugins=(
-  gitfast
-  last-working-dir
-  common-aliases
-  sublime
-  zsh-syntax-highlighting
-  history-substring-search
-  docker
-  docker-compose
-  wd
-  # zsh-autosuggestions
+    gitfast
+    last-working-dir
+    common-aliases
+    sublime
+    zsh-syntax-highlighting
+    history-substring-search
+    docker
+    docker-compose
+    wd
+    # zsh-autosuggestions
 )
 
 # exa ls
