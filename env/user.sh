d6366990c6b1c8a6d8d07e5ba39ca6fe506cb8f6
richard jarram <rjarram@me.com>
2024-02-20 15:54:48 +0900

    reorder

--- env/user.sh
+++ env/user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export CHEATS="/Users/richardjarram/code/cheatsheets"
 export CODE_DIR="/Users/richardjarram/code"
+
+export CHEATS="/Users/richardjarram/code/cheatsheets"
 export DOTFILES_DIR="$CODE_DIR/metalogica/dotfiles"
 export GTD_DIR="/Users/richardjarram/Documents/GTD"
 export RETOOL_DIR="/Users/richardjarram/code/shakepay/retool"
