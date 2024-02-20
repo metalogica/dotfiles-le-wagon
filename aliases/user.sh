81739da1a15ea701c97167ea26eb4826aa151a09
richard jarram <rjarram@me.com>
2024-02-21 00:45:17 +0900

    add cicd

--- aliases/user.sh
+++ aliases/user.sh
@@ -2,3 +2,4 @@
 alias dotfiles="code \$DOTFILES_DIR"
 alias shakepay_backend="code \$SHAKEPAY_DIR/shake-hosted-wallet"
 alias shakepay_app="code \$SHAKEPAY_DIR/shaketh"
+alias shakepay_cicd="code \$SHAKEPAY_DIR/shake-cicd"
