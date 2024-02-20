7965e3a2f25797f22722f921fcc4733f1b595f7d
richard jarram <rjarram@me.com>
2024-02-20 15:54:03 +0900

    re-order

--- functions/shakepay.sh
+++ functions/shakepay.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-# run a single test
-stest() {
-    DATABASE_URL=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_REPORTING=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_REPORTING_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_MIGRATIONS=postgres://postgres:@127.0.0.1:5432/shakepay_test APP_TYPE=shakeboard LIGHTSHIP_PORT=5005 NODE_ENV=test TS_NODE_TRANSPILE_ONLY=true npx -w apps/backend mocha --config /Users/richardjarram/code/shakepay/shake-hosted-wallet/apps/backend/.mocharc.dev.json `echo "$1"`
+# tag build
+function create_shaketh_tag() {
+    qa_prefix=$1
+    version_number=$2
+    
+    current_branch=$(git branch --show-current)
+    tag="${qa_prefix}-${current_branch}-${version_number}"
+    
+    git tag $tag
+    
+    git push origin $tag
+    
+    echo "Successfully started new Cricle CI build for: ${tag}"
 }
 
 # run devtest in test container
-devtest() {
+function devtest() {
     DATABASE_URL=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_REPORTING=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_REPORTING_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_MIGRATIONS=postgres://postgres:@127.0.0.1:5432/shakepay_test APP_TYPE=shakeboard LIGHTSHIP_PORT=5005 NODE_ENV=test npm run -w apps/backend devmocha
 }
 
-# jump scripts
-sj () {
-    if [[ $1 = 'mq' ]]
-    then
-        $JUMP_SCRIPT shakepay-dev.marqeta.com 443 9090
-        
-    elif [[ $1 = 'qa' ]]
-    then
-        $JUMP_SCRIPT backendqa-cluster.cluster-criaf4lhc3h5.ca-central-1.rds.amazonaws.com 5432 15432
-        
-    elif [[ $1 = 'qa2' ]]
-    then
-        $JUMP_SCRIPT backendqa2-cluster.cluster-criaf4lhc3h5.ca-central-1.rds.amazonaws.com 5432 15432
-        
-    fi
-}
-
 # shakepay AWS aliases
-getsecret() {
+function getsecret() {
     if [[ -z "${1}" ]]; then
         echo "Please provide the secret ID as an argument."
         return 1
@@ -36,6 +29,7 @@ getsecret() {
     aws secretsmanager get-secret-value --secret-id "${1}" --query SecretString
 }
 
+# build library
 function shakepay_build_workspace() {
     if [ "$#" -eq 0 ]; then
         echo "Usage: shakepay_build_wokspace WORKSPACE [WORKSPACE...]"
@@ -53,16 +47,24 @@ function shakepay_build_workspace() {
     done
 }
 
-function create_shaketh_tag() {
-    qa_prefix=$1
-    version_number=$2
-    
-    current_branch=$(git branch --show-current)
-    tag="${qa_prefix}-${current_branch}-${version_number}"
-    
-    git tag $tag
-    
-    git push origin $tag
-    
-    echo "Successfully started new Cricle CI build for: ${tag}"
+# jump scripts
+function sj () {
+    if [[ $1 = 'mq' ]]
+    then
+        $JUMP_SCRIPT shakepay-dev.marqeta.com 443 9090
+        
+    elif [[ $1 = 'qa' ]]
+    then
+        $JUMP_SCRIPT backendqa-cluster.cluster-criaf4lhc3h5.ca-central-1.rds.amazonaws.com 5432 15432
+        
+    elif [[ $1 = 'qa2' ]]
+    then
+        $JUMP_SCRIPT backendqa2-cluster.cluster-criaf4lhc3h5.ca-central-1.rds.amazonaws.com 5432 15432
+        
+    fi
+}
+
+# run a single test
+function stest() {
+    DATABASE_URL=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_test DATABASE_URL_REPORTING=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_REPORTING_READONLY=postgres://postgres:@127.0.0.1:5432/shakepay_reporting_test DATABASE_URL_MIGRATIONS=postgres://postgres:@127.0.0.1:5432/shakepay_test APP_TYPE=shakeboard LIGHTSHIP_PORT=5005 NODE_ENV=test TS_NODE_TRANSPILE_ONLY=true npx -w apps/backend mocha --config /Users/richardjarram/code/shakepay/shake-hosted-wallet/apps/backend/.mocharc.dev.json `echo "$1"`
 }
