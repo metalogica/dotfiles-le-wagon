0987fc94f23ff53082d5c62d320ef6000bbf08e7
Richard Jarram <43706901+metalogica@users.noreply.github.com>
2024-04-03 10:01:32 -0400

    shakepay web alias

--- aliases/user.sh
+++ aliases/user.sh
@@ -2,6 +2,7 @@
 alias dotfiles="code \$DOTFILES_DIR"
 alias shakepay_backend="code \$SHAKEPAY_DIR/shake-hosted-wallet"
 alias shakepay_app="code \$SHAKEPAY_DIR/shaketh"
+alias shakepay_web="code \$SHAKEPAY_DIR/shake-web"
 alias shakepay_cicd="code \$SHAKEPAY_DIR/shake-ci-cd"
 
 # Git
